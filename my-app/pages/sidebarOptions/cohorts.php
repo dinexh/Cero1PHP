f7355ad5f3bde66667f6c9edeb57bb0aee1c03a0
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-10-15 12:30:13 +0530

    removed unwanted stuff

--- my-app/pages/sidebarOptions/cohorts.php
+++ my-app/pages/sidebarOptions/cohorts.php
@@ -1,27 +1,6 @@
-<?php
-session_start();
-require_once(__DIR__ . '/../../../config.php');
-if (isset($_SESSION['role'])) {
-    $userRole = $_SESSION['role'];
-} else {
-    header("Location: ../index.php"); 
-    exit();
-}
-if (!isset($_SESSION['id_number'])) {
-    header("Location: index.php"); 
-    exit();
-}
-
-$pageTitle = 'Cohorts';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="content">
