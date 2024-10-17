c70acf2fb60f1c7acdd75dd08e1f9913b8e610ec
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-10-17 13:53:05 +0530

    added links to footer

--- my-app/includes/footer.php
+++ my-app/includes/footer.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../../includes/footer.css">
 <div class="footer">
     <div class="footer-in">
-        <p>&copy; 2024 All rights Reservesd by <a href="/">ZeroOne Code Club</a> | KLEF SAC</p>
-        <p>Designed and Developed by <a href="/">Dinesh Korukonda</a></p>
+        <p>&copy; 2024 All rights Reservesd by <a href="https://klzeroone.vercel.app">ZeroOne Code Club</a> | KLEF SAC</p>
+        <p>Designed and Developed by <a href="https://dineshkorukonda.in">Dinesh Korukonda</a></p>
     </div>
 </div>
